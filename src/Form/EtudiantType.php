20c37feb9f65ced040baa91c7bb57a9fae33781e
pierclaud7 <pierclaudbouki18@gmail.com>
2026-01-27 22:17:02 +0100

    Ajout des champs nom, prénom et email au formulaire d'étudiant avec des labels appropriés

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -6,6 +6,9 @@ use App\Entity\Etudiant;
 use App\Entity\Session;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +17,27 @@ class EtudiantType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('prenom')
-            ->add('moyenne')
+            ->add('nom', TextType::class, ['label' => 'Nom'])
+            ->add('prenom', TextType::class, ['label' => 'Prénom'])
+            ->add('email', EmailType::class, ['label' => 'Adresse Email'])
+
             ->add('session', EntityType::class, [
                 'class' => Session::class,
-                'choice_label' => 'id',
+                'label' => 'Session d\'examen',
+                'choice_label' => function (Session $session) {
+                    return $session->getDiplome()->getIntitule() . ' - ' . $session->getAnnee();
+                },
+            ])
+
+            ->add('resultat', ChoiceType::class, [
+                'label' => 'Résultat',
+                'required' => false,
+                'placeholder' => 'En attente de résultat...',
+                'choices'  => [
+                    'Admis' => 'Admis',
+                    'Rattrapage' => 'Rattrapage',
+                    'Refusé' => 'Refusé',
+                ],
             ])
         ;
     }
