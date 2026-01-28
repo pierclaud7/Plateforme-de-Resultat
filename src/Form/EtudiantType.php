b78c856065d30cbc4989e6eebc88eaf55fe73d9e
pierclaud7 <pierclaudbouki18@gmail.com>
2026-01-28 14:11:21 +0100

    Amélioration du formulaire étudiant : mise à jour des types de champs, ajout du champ Date de naissance et ajustement des labels.

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -6,8 +6,10 @@ use App\Entity\Etudiant;
 use App\Entity\Session;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType; // Pour la case à cocher
-use Symfony\Component\Form\Extension\Core\Type\NumberType;   // Pour la moyenne
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -23,30 +25,33 @@ class EtudiantType extends AbstractType
             ->add('prenom', TextType::class, [
                 'label' => 'Prénom'
             ])
-            ->add('email', TextType::class, [
+            ->add('email', EmailType::class, [
                 'label' => 'Adresse Email'
             ])
-            // Ajout du champ Session (Menu déroulant)
+
+            ->add('dateNaissance', DateType::class, [
+                'label' => 'Date de naissance',
+                'widget' => 'single_text',
+                'required' => false
+            ])
+
             ->add('session', EntityType::class, [
                 'class' => Session::class,
                 'choice_label' => function (Session $session) {
-                    // Affiche "BTS SIO - 2025" dans la liste
                     return $session->getDiplome()->getIntitule() . ' - ' . $session->getAnnee();
                 },
                 'label' => 'Session d\'examen',
                 'placeholder' => 'Choisir une session...'
             ])
-            // Ajout du champ Moyenne
             ->add('moyenne', NumberType::class, [
                 'label' => 'Moyenne obtenue',
-                'required' => false, // Pas obligatoire à l'inscription
-                'scale' => 2, // 2 chiffres après la virgule
+                'required' => false,
+                'scale' => 2,
                 'attr' => ['placeholder' => 'Ex: 14.50']
             ])
-            // Ajout de la case "Admis"
             ->add('estAdmis', CheckboxType::class, [
                 'label' => 'L\'étudiant est-il admis ?',
-                'required' => false, // Une case à cocher n'est jamais "obligatoire" au sens HTML
+                'required' => false,
             ])
         ;
     }
