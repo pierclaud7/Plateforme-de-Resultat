ff9b8e9c238779bb34887bcda1484c2e1fddd723
pierclaud7 <pierclaudbouki18@gmail.com>
2026-01-29 16:32:14 +0100

    création de l'entité User conforme aux standards Symfony Security

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -95,14 +95,4 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * Ensure the session doesn't contain actual password hashes by CRC32C-hashing them, as supported since Symfony 7.3.
-     */
-    public function __serialize(): array
-    {
-        $data = (array) $this;
-        $data["\0".self::class."\0password"] = hash('crc32c', $this->password);
-
-        return $data;
-    }
 }
